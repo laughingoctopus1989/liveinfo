b279b7bb4847a176b91f6286857c44dec32bb347
laughingoctopus1989 <hehek@tuta.io>
2018-10-25 21:29:13 -0400

    everything works so far, will have to test on other OS's

--- liveinfo.py
+++ liveinfo.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
-import platform
-
-print(platform.uname())
+import os, re, string
+uname = re.findall(r"'([^']*)'" , str(os.uname()))
+#some regex i found that gets data in quotes from uname
+ostype = uname[0]
+hostname = uname[1]
+ops = re.search(r"[A-Z]+", uname[2])
+ops = ops.group()
+if uname[-1] == 'x86_64':
+    bit = 'x64'
+else:
+    bit = 'x32'
+print(
+"""
+Live Information---\n
+Operating System: %s %s
+Host Name: %s
+Architecture: %s
+""" % (ops, ostype, hostname, bit))
