336a04c9f052746c18ccf477b7e420a3614371a8
laughingoctopus1989 <hehek@tuta.io>
2018-11-01 12:35:26 -0400

    On branch master
    	modified:   liveinfo.py
    actually functional and works on different operating system
    who would have known that using platform was way better

--- liveinfo.py
+++ liveinfo.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+import platform
+print("""
+ _    _____ _   _ _____ _____ _   _______ _____
+| |  |_   _| | | |  ___|_   _| \ | |  ___|  _  |
+| |    | | | | | | |__   | | |  \| | |_  | | | |
+| |    | | | | | |  __|  | | | . ` |  _| | | | |
+| |____| |_\ \_/ / |___ _| |_| |\  | |   \ \_/ /
+\_____|___/ \___/\____/ \___/\_| \_|_|    \___/
+
+                                                
+Operating System: %s\n
+Processor: %s\n
+Hostname: %s\n
+""" % (platform.system(), platform.processor(), platform.node()))
